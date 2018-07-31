9522cc5972474ea3c9bcb6fe4895a562c42f3554
True Entrant <trueentrant@gmail.com>
2018-07-31 12:16:44 +0300

    Updates

--- src/redusers/index.js
+++
@@ -1,7 +0,0 @@
-import {combineReducers} from "redux";
-import * as regLogReduser from './reg-login.redusers'
-
-
-export const rootReducer = combineReducers({
-    usersData: regLogReduser.reducer
-});
